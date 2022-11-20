d1a315926b89e7c78192a0b66025857417b52879
gwhitney15 <gwhitney115@gmail.com>
2022-11-19 19:08:03 -0600

    added user model and some routes plus signtoken

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,29 @@
+const User = require('../models/user');
+
+const { signToken } = require('../utils/auth');
+
+module.exports = {
+    async createUser({ body }, res) {
+        const user = await User.create(body);
+
+        if (!user) {
+            return res.status(400).json({ message: 'Something is wrong!' });
+        }
+        const token = signToken(user);
+        res.json({ token, user });
+    },
+    async login({ body }, res) {
+        const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+        if (!user) {
+            return res.status(400).json({ message: "Can't find this user" });
+        }
+
+        const correctPw = await body.password;
+
+        if (!correctPw) {
+            return res.status(400).json({ message: 'Wrong password!' });
+        }
+        const token = signToken(user);
+        res.json({ token, user });
+    }
+}
