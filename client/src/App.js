4c4219476d75d9e4c861b9312edc4995f40b096e
Derek Kilgore <derek.kilgore@gmail.com>
2022-11-25 19:29:25 -0600

    starting

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './node_modules/bootstrap/dist/css/bootstrap.css'
+import './node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Login from './component/login'
