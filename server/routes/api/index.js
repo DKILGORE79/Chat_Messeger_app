a31d6bfe8289339dee2e37061e60852c12837a7c
Dakota Jones <dakota32jones@gmail.com>
2022-11-25 19:24:30 -0600

    added models and schema

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,8 @@
-const router = require('express').Router();
-const userRoutes = require('./user-routes');
+const router = require("express").Router();
+const userRoutes = require("./user-routes");
+const interestsRoutes = require("./interestsRoutes");
+router.use("/users", userRoutes);
 
-router.use('/users', userRoutes);
+router.use("/Interests", interestsRoutes);
 
 module.exports = router;
