ac54637e00634510ab0487ea97ebb79ea4d3586f
gwhitney15 <gwhitney115@gmail.com>
2022-11-28 03:26:08 -0600

    chat app + login stuff

---
+++ client/src/components/room.js
@@ -0,0 +1,16 @@
+
+
+
+function VideoChat() {
+
+    return (<div><head>
+    </head>
+        <body>
+            <div id="video-grid">hello</div>
+            <div id="video">hello</div>
+            <button></button>
+        </body></div>)
+};
+
+
+export default VideoChat;
