5a7915113a33d117ec8931c8649abe0631750c3b
Derek Kilgore <derek.kilgore@gmail.com>
2022-11-29 18:18:34 -0600

    1129616

--- New folder/client/src/components/InitialSignUp.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from "react";
-import QuizComponent from "./QuizComponent";
-
-function InitialSignUp() {
-  return <QuizComponent />;
-}
-
-export default InitialSignUp;
